bda0dc636e98c1027ec6a2c590906c9c6cb4b4da
Andrea Delli <andrea.delli2001@gmail.com>
2023-01-20 16:20:35 +0100

    refactor(ex1): convert bst insert from recursive to iterative

--- Esercizio 1/binary_search_tree.py
+++ Esercizio 1/binary_search_tree.py
@@ -1,6 +1,7 @@
 from typing import Callable
 from node import Node
 
+
 class BST:
     """ Binary Search Tree """
 
@@ -8,29 +9,24 @@ class BST:
         """ Constructor """
         self.root: Node | None = None
 
-    def set_root(self, new_root: Node):
-        """ Set the root  of the tree """
-        self.root = new_root
-
     def insert(self, key: int):
         """ Insert a new key into the tree """
-        if self.root is None:
-            self.set_root(Node(key))
+        new_node = Node(key)
+        parent = None
+        navigator = self.root
+        # Search the correct position for the new node iteratively
+        while navigator is not None:  # If it's None, I found a leaf position
+            parent = navigator
+            if new_node.key < navigator.key:
+                navigator = navigator.left
+            else:
+                navigator = navigator.right
+        if parent is None:  # Tree was empty
+            self.root = new_node
+        elif new_node.key < parent.key:
+            parent.left = new_node
         else:
-            self.insert_node(self.root, Node(key))
-
-    def insert_node(self, current_node: Node, new_node: Node):
-        """ Find the correct position for the key and insert a new node into the tree """
-        if (new_node.key <= current_node.key):
-            if (current_node.left):
-                self.insert_node(current_node.left, new_node)
-            else:
-                current_node.left = new_node
-        elif (new_node.key > current_node.key):
-            if (current_node.right):
-                self.insert_node(current_node.right, new_node)
-            else:
-                current_node.right = new_node
+            parent.right = new_node
 
     def find(self, key: int):
         """
